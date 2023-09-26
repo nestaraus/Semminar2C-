0b7bf080f612cbddbbc5e620afcd9a1f7a494b01
nestaraus <nestaraus@mail.pu>
2023-09-26 20:40:53 +0300

    Figurnayainterpolyaziya

---
+++ 002/Program.cs
@@ -0,0 +1,6 @@
+﻿string name = "Rustem";
+int age = 40;
+string city = "Krasnodar";
+
+// Console.WriteLine("Меня зовут:" + name + " и мне " + age + " лет. Я из " + city);
+Console.WriteLine($"Меня зовут: {name} и мне {age} лет. Я из {city}");

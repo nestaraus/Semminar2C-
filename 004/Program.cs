89a7969765cec1f1f2a15738aec469b7c6c1a9c7
nestaraus <nestaraus@mail.pu>
2023-09-26 21:21:36 +0300

    kratnost

---
+++ 004/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n%7==0 && n%23==0 )
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
